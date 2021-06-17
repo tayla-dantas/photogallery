95cda8d9cb795516275c2d98bc78429880548626
TAYLA TINEU <dantas.tayla@gmail.com>
2021-06-17 17:19:24 -0300

    File upload feature

--- BotoApi.py
+++ BotoApi.py
@@ -1,15 +1,33 @@
-import boto3
+import boto3, uuid, base64
+
+_DEFAULT_BUCKET_NAME='galleryforafriend'
+_DEFAULT_BUCKET_LOCATION='sa-east-1'
 
 s3_resource = boto3.resource('s3',aws_access_key_id='AKIASZ6M73INSE3WJPM4',
          aws_secret_access_key='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')
 
-bucket = s3_resource.Bucket(name='galleryforafriend')
+bucket = s3_resource.Bucket(name=_DEFAULT_BUCKET_NAME)
 
-bucketObject = s3_resource.Object(
-    bucket_name='galleryforafriend', 
-    key='testPhoto.jpg'
-)
 
-bucketObject.upload_file('testPhoto.jpg')
+def awsUpload(base64String): 
+    FILE_NAME = str(uuid.uuid4()) + '.jpg'
+    bucketObject = s3_resource.Object(
+    bucket_name=_DEFAULT_BUCKET_NAME, 
+    key=FILE_NAME)
+    bucketObject.put(Body=base64.b64decode(base64String))
+    #bucketObject.upload_file('testPhoto.jpg')
+    object_url = "https://%s.s3-%s.amazonaws.com/%s" % (_DEFAULT_BUCKET_NAME,_DEFAULT_BUCKET_LOCATION, FILE_NAME)
+    print(object_url)
+    return 'upload ok'
 
-print('upload ok!')
+def awsDownload():
+    images = ()
+
+    for bucketObject in bucket.objects.all():
+        object_url = "https://%s.s3-%s.amazonaws.com/%s" % (_DEFAULT_BUCKET_NAME,_DEFAULT_BUCKET_LOCATION, bucketObject.key)
+        #images.append(object_url)
+        #images.add(object_url)
+        imagesList = list(images)
+        imagesList.append(object_url)
+        images = tuple(imagesList)
+    return images
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, url_for, render_template
 from flask_bootstrap import Bootstrap
+import base64, json, BotoApi
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -7,7 +8,21 @@ Bootstrap(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", images=BotoApi.awsDownload())
+
+@app.route("/store",  methods = ['POST'] )
+def store():
+    json_data = request.json
+    base64String = json_data["img"]
+    image = base64String.replace("data:image/jpeg;base64,", "")
+    BotoApi.awsUpload(image)
+    return "JSON value sent: " + image
+
+@app.route("/retrieve")
+def retrieve():
+    jsonString = json.dumps(BotoApi.awsDownload())
+    return jsonString
+
 
 if __name__ == "__main__":
     app.run(debug=True)
